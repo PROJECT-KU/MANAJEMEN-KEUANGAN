543af1de2dbc18c69e7b321fdc7aabf178b12379
berto juni <bertojunikrisnanto@gmail.com>
2023-10-31 10:15:14 +0700

    fix || fix presensi update 10 || 1.12.1

--- resources/views/account/presensi/create.blade.php
+++ resources/views/account/presensi/create.blade.php
@@ -231,173 +231,173 @@ Tambah Presensi Karyawan | MANAGEMENT
         ?>
         @if (date('H:i:s') >= '22:00:00' && date('H:i:s') <= '23:59:59' ) <button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
           @elseif ($currentDay == 1 && ($currentTime >= '00:00:00' && $currentTime <= '07:59:59' )) <button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
-            <button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
-            @elseif ($currentDay == 4 && ($currentTime >= '00:00:00' && $currentTime <= '11:59:59' )) <button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
-              @elseif (in_array($currentDay, [5, 6, 7]) && ($currentTime >= '00:00:00' && $currentTime <= '06:59:59' )) <button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
-                @else
-                <button class="btn btn-primary mr-1 btn-submit" type="submit"><i class="fa fa-paper-plane"></i> SIMPAN</button>
-                @endif
+            @elseif (in_array($currentDay, [2, 3]) && ($currentTime >= '00:00:00' && $currentTime <= '23:59:59' ))<button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
+              @elseif ($currentDay == 4 && ($currentTime >= '00:00:00' && $currentTime <= '11:59:59' )) <button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
+                @elseif (in_array($currentDay, [5, 6, 7]) && ($currentTime >= '00:00:00' && $currentTime <= '06:59:59' )) <button class="btn btn-secondary mr-1 btn-submit" type="submit" disabled><i class="fa fa-paper-plane"></i> SIMPAN</button>
+                  @else
+                  <button class="btn btn-primary mr-1 btn-submit" type="submit"><i class="fa fa-paper-plane"></i> SIMPAN</button>
+                  @endif
 
-                <button class="btn btn-warning btn-reset" type="reset"><i class="fa fa-redo"></i> RESET</button>
-                </form>
+                  <button class="btn btn-warning btn-reset" type="reset"><i class="fa fa-redo"></i> RESET</button>
+                  </form>
 
-                </div>
-                </div>
-                </section>
-                </div>
+                  </div>
+                  </div>
+                  </section>
+                  </div>
 
-                <!-- live lokasi -->
-                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-                <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-                <script>
-                  function initMap() {
-                    if ("geolocation" in navigator) {
-                      const options = {
-                        enableHighAccuracy: true, // Request high-accuracy location data
-                      };
+                  <!-- live lokasi -->
+                  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+                  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+                  <script>
+                    function initMap() {
+                      if ("geolocation" in navigator) {
+                        const options = {
+                          enableHighAccuracy: true, // Request high-accuracy location data
+                        };
 
-                      const map = L.map('map').setView([0, 0], 16); // Initial view
-                      let marker = null; // Initialize marker
+                        const map = L.map('map').setView([0, 0], 16); // Initial view
+                        let marker = null; // Initialize marker
 
-                      // Set up the map
-                      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                      }).addTo(map);
+                        // Set up the map
+                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        }).addTo(map);
 
-                      navigator.geolocation.watchPosition(
-                        function(position) {
-                          const latitude = position.coords.latitude;
-                          const longitude = position.coords.longitude;
+                        navigator.geolocation.watchPosition(
+                          function(position) {
+                            const latitude = position.coords.latitude;
+                            const longitude = position.coords.longitude;
 
-                          // Update the input fields with the new location
-                          document.getElementById('latitude').value = latitude;
-                          document.getElementById('longitude').value = longitude;
+                            // Update the input fields with the new location
+                            document.getElementById('latitude').value = latitude;
+                            document.getElementById('longitude').value = longitude;
 
-                          // Update the map with the new location
-                          if (marker) {
-                            map.removeLayer(marker);
-                          }
-                          marker = L.marker([latitude, longitude]).addTo(map);
-                          marker.bindPopup('Lokasi Anda').openPopup();
-                          map.setView([latitude, longitude]);
-                        },
-                        function(error) {
-                          console.log(`Error getting location: ${error.message}`);
-                          // Handle location errors here, e.g., display an error message to the user.
-                        },
-                        options
-                      );
-                    } else {
-                      alert('Geolocation tidak didukung oleh browser Anda.');
-                    }
-                  }
-
-                  // Panggil fungsi initMap() saat halaman dimuat
-                  window.onload = initMap;
-                </script>
-
-                <!-- Style untuk peta (gunakan CSS sesuai dengan preferensi Anda) -->
-                <style>
-                  #map {
-                    width: 100%;
-                    height: 400px;
-                  }
-                </style>
-                <!-- end live lokasi -->
-
-                <!-- maksimal upload gambar & jenis file yang di perbolehkan -->
-                <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                <script>
-                  document.getElementById('gambar').addEventListener('change', function() {
-                    const maxFileSizeInBytes = 5024 * 5024; // 5MB
-                    const allowedExtensions = ['jpg', 'jpeg', 'png'];
-                    const fileInput = this;
-
-                    if (fileInput.files.length > 0) {
-                      const selectedFile = fileInput.files[0];
-                      const fileSize = selectedFile.size; // Get the file size in bytes
-                      const fileName = selectedFile.name.toLowerCase();
-
-                      // Check file size
-                      if (fileSize > maxFileSizeInBytes) {
-                        // Display a SweetAlert error message
-                        Swal.fire({
-                          icon: 'error',
-                          title: 'Ukuran File Melebihi Batas',
-                          text: 'Ukuran File Yang Diperbolehkan Dibawah 5MB.',
-                        });
-                        fileInput.value = ''; // Clear the file input
-                        return;
-                      }
-
-                      // Check file extension
-                      const fileExtension = fileName.split('.').pop();
-                      if (!allowedExtensions.includes(fileExtension)) {
-                        // Display a SweetAlert error message
-                        Swal.fire({
-                          icon: 'error',
-                          title: 'Jenis File Tidak Valid',
-                          text: 'Hanya File JPG, JPEG, dan PNG Yang Diperbolehkan.',
-                        });
-                        fileInput.value = ''; // Clear the file input
+                            // Update the map with the new location
+                            if (marker) {
+                              map.removeLayer(marker);
+                            }
+                            marker = L.marker([latitude, longitude]).addTo(map);
+                            marker.bindPopup('Lokasi Anda').openPopup();
+                            map.setView([latitude, longitude]);
+                          },
+                          function(error) {
+                            console.log(`Error getting location: ${error.message}`);
+                            // Handle location errors here, e.g., display an error message to the user.
+                          },
+                          options
+                        );
+                      } else {
+                        alert('Geolocation tidak didukung oleh browser Anda.');
                       }
                     }
-                  });
-                </script>
-                <!-- end -->
 
-                <!-- upload image -->
-                <script>
-                  const imageInput = document.getElementById('gambar');
-                  const imagePreview = document.getElementById('image-preview');
+                    // Panggil fungsi initMap() saat halaman dimuat
+                    window.onload = initMap;
+                  </script>
 
-                  imageInput.addEventListener('change', (e) => {
-                    const file = e.target.files[0];
-                    if (file) {
-                      const reader = new FileReader();
-                      reader.onload = (e) => {
-                        imagePreview.src = e.target.result;
-                        imagePreview.style.display = 'block'; // Show the preview
-                      };
-                      reader.readAsDataURL(file);
+                  <!-- Style untuk peta (gunakan CSS sesuai dengan preferensi Anda) -->
+                  <style>
+                    #map {
+                      width: 100%;
+                      height: 400px;
                     }
-                  });
-                </script>
-                <!-- end upload image -->
+                  </style>
+                  <!-- end live lokasi -->
 
-                <!-- Include CKEditor JS -->
-                <script src="//cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
-                <script>
-                  CKEDITOR.replace('note');
-                </script>
-                <!-- end ckeditor -->
+                  <!-- maksimal upload gambar & jenis file yang di perbolehkan -->
+                  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                  <script>
+                    document.getElementById('gambar').addEventListener('change', function() {
+                      const maxFileSizeInBytes = 5024 * 5024; // 5MB
+                      const allowedExtensions = ['jpg', 'jpeg', 'png'];
+                      const fileInput = this;
 
-                <script>
-                  /**
-                   * btn submit loader
-                   */
-                  $(".btn-submit").click(function() {
-                    $(".btn-submit").addClass('btn-progress');
-                    if (timeoutHandler) clearTimeout(timeoutHandler);
+                      if (fileInput.files.length > 0) {
+                        const selectedFile = fileInput.files[0];
+                        const fileSize = selectedFile.size; // Get the file size in bytes
+                        const fileName = selectedFile.name.toLowerCase();
 
-                    timeoutHandler = setTimeout(function() {
-                      $(".btn-submit").removeClass('btn-progress');
+                        // Check file size
+                        if (fileSize > maxFileSizeInBytes) {
+                          // Display a SweetAlert error message
+                          Swal.fire({
+                            icon: 'error',
+                            title: 'Ukuran File Melebihi Batas',
+                            text: 'Ukuran File Yang Diperbolehkan Dibawah 5MB.',
+                          });
+                          fileInput.value = ''; // Clear the file input
+                          return;
+                        }
 
-                    }, 1000);
-                  });
+                        // Check file extension
+                        const fileExtension = fileName.split('.').pop();
+                        if (!allowedExtensions.includes(fileExtension)) {
+                          // Display a SweetAlert error message
+                          Swal.fire({
+                            icon: 'error',
+                            title: 'Jenis File Tidak Valid',
+                            text: 'Hanya File JPG, JPEG, dan PNG Yang Diperbolehkan.',
+                          });
+                          fileInput.value = ''; // Clear the file input
+                        }
+                      }
+                    });
+                  </script>
+                  <!-- end -->
 
-                  /**
-                   * btn reset loader
-                   */
-                  $(".btn-reset").click(function() {
-                    $(".btn-reset").addClass('btn-progress');
-                    if (timeoutHandler) clearTimeout(timeoutHandler);
+                  <!-- upload image -->
+                  <script>
+                    const imageInput = document.getElementById('gambar');
+                    const imagePreview = document.getElementById('image-preview');
 
-                    timeoutHandler = setTimeout(function() {
-                      $(".btn-reset").removeClass('btn-progress');
+                    imageInput.addEventListener('change', (e) => {
+                      const file = e.target.files[0];
+                      if (file) {
+                        const reader = new FileReader();
+                        reader.onload = (e) => {
+                          imagePreview.src = e.target.result;
+                          imagePreview.style.display = 'block'; // Show the preview
+                        };
+                        reader.readAsDataURL(file);
+                      }
+                    });
+                  </script>
+                  <!-- end upload image -->
 
-                    }, 500);
-                  })
-                </script>
+                  <!-- Include CKEditor JS -->
+                  <script src="//cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
+                  <script>
+                    CKEDITOR.replace('note');
+                  </script>
+                  <!-- end ckeditor -->
 
-                @stop
+                  <script>
+                    /**
+                     * btn submit loader
+                     */
+                    $(".btn-submit").click(function() {
+                      $(".btn-submit").addClass('btn-progress');
+                      if (timeoutHandler) clearTimeout(timeoutHandler);
+
+                      timeoutHandler = setTimeout(function() {
+                        $(".btn-submit").removeClass('btn-progress');
+
+                      }, 1000);
+                    });
+
+                    /**
+                     * btn reset loader
+                     */
+                    $(".btn-reset").click(function() {
+                      $(".btn-reset").addClass('btn-progress');
+                      if (timeoutHandler) clearTimeout(timeoutHandler);
+
+                      timeoutHandler = setTimeout(function() {
+                        $(".btn-reset").removeClass('btn-progress');
+
+                      }, 500);
+                    })
+                  </script>
+
+                  @stop
