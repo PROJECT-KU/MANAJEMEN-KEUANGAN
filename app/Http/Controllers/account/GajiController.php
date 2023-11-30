2ff22966729db3080e8f927dc13cf8971034de86
berto juni <bertojunikrisnanto@gmail.com>
2023-11-30 21:43:18 +0700

    fix sementara 3

--- app/Http/Controllers/account/GajiController.php
+++ app/Http/Controllers/account/GajiController.php
@@ -814,56 +814,86 @@ class GajiController extends Controller
     }
   }
 
+  // public function downloadPdf(Request $request)
+  // {
+  //   $user = Auth::user();
+  //   $startDate = $request->input('tanggal_awal');
+  //   $endDate = $request->input('tanggal_akhir');
+
+  //   if (!$startDate || !$endDate) {
+  //     // Jika tanggal_awal atau tanggal_akhir tidak ada dalam request, gunakan rentang bulan ini
+  //     $currentMonth = date('Y-m-01 00:00:00');
+  //     $nextMonth = date('Y-m-01 00:00:00', strtotime('+1 month'));
+  //   } else {
+  //     // Jika tanggal_awal dan tanggal_akhir ada dalam request, gunakan rentang tersebut
+  //     $currentMonth = date('Y-m-d 00:00:00', strtotime($startDate));
+  //     $nextMonth = date('Y-m-d 00:00:00', strtotime($endDate));
+  //   }
+
+  //   if ($user->level == 'manager' || $user->level == 'staff') {
+  //     $gaji = DB::table('gaji')
+  //       ->select('gaji.id', 'gaji.id_transaksi', 'gaji.gaji_pokok', 'gaji.lembur', 'gaji.bonus', 'gaji.tunjangan', 'gaji.tanggal', 'gaji.pph', 'gaji.total', 'gaji.status', 'users.id as user_id', 'users.full_name as full_name', 'users.nik as nik', 'users.norek as norek', 'users.bank as bank')
+  //       ->leftJoin('users', 'gaji.user_id', '=', 'users.id')
+  //       ->where('users.company', $user->company)
+  //       ->whereBetween('gaji.tanggal', [$currentMonth, $nextMonth])
+  //       ->orderBy('gaji.created_at', 'DESC')
+  //       ->get();
+  //   } else if ($user->level == 'karyawan' || $user->level == 'trainer') {
+  //     $gaji = DB::table('gaji')
+  //       ->select('gaji.id', 'gaji.id_transaksi', 'gaji.gaji_pokok', 'gaji.lembur', 'gaji.bonus', 'gaji.tunjangan', 'gaji.tanggal', 'gaji.pph', 'gaji.total', 'gaji.status', 'users.id as user_id', 'users.full_name as full_name', 'users.nik as nik', 'users.norek as norek', 'users.bank as bank')
+  //       ->leftJoin('users', 'gaji.user_id', '=', 'users.id')
+  //       ->where('gaji.user_id', $user->id)
+  //       ->whereBetween('gaji.tanggal', [$currentMonth, $nextMonth])
+  //       ->orderBy('gaji.created_at', 'DESC')
+  //       ->get();
+  //   } else {
+  //     $gaji = Gaji::select('gaji.*', 'users.name as full_name')
+  //       ->join('users', 'gaji.user_id', '=', 'users.id')
+  //       ->where('gaji.user_id', $user->id)
+  //       ->whereBetween('gaji.tanggal', [$currentMonth, $nextMonth])
+  //       ->orderBy('gaji.created_at', 'DESC')
+  //       ->get();
+  //   }
+
+  //   // Calculate total gaji
+  //   $totalGaji = $gaji->sum('total');
+  //   $terbilang = Terbilang::make($totalGaji, ' rupiah');
+  //   $users = User::all(); // Get all users
+
+  //   // total gaji yang status terbayar
+  //   $gajiTerbayar = $gaji->where('status', 'terbayar');
+  //   $totalGajiTerbayar = $gajiTerbayar->sum('total');
+  //   $terbilangterbayar = Terbilang::make($totalGajiTerbayar, ' rupiah');
+
+  //   // Get the HTML content of the view
+  //   $html = view('account.gaji.pdf', compact('gaji', 'totalGaji', 'user', 'terbilang', 'startDate', 'endDate', 'totalGajiTerbayar', 'terbilangterbayar'))->render();
+
+  //   // Instantiate Dompdf with the default configuration
+  //   $dompdf = new Dompdf();
+
+  //   // Load the HTML content into Dompdf
+  //   $dompdf->loadHtml($html);
+
+  //   // (Optional) Set paper size and orientation
+  //   $dompdf->setPaper('A4', 'landscape');
+
+  //   // Render the PDF
+  //   $dompdf->render();
+
+  //   // Set the PDF filename
+  //   $fileName = 'List-Gaji-Karyawan_' . date('d-m-Y') . '.pdf';
+
+  //   // Output the generated PDF to the browser
+  //   return $dompdf->stream($fileName);
+  // }
+
   public function downloadPdf(Request $request)
   {
     $user = Auth::user();
     $startDate = $request->input('tanggal_awal');
     $endDate = $request->input('tanggal_akhir');
 
-    if (!$startDate || !$endDate) {
-      // Jika tanggal_awal atau tanggal_akhir tidak ada dalam request, gunakan rentang bulan ini
-      $currentMonth = date('Y-m-01 00:00:00');
-      $nextMonth = date('Y-m-01 00:00:00', strtotime('+1 month'));
-    } else {
-      // Jika tanggal_awal dan tanggal_akhir ada dalam request, gunakan rentang tersebut
-      $currentMonth = date('Y-m-d 00:00:00', strtotime($startDate));
-      $nextMonth = date('Y-m-d 00:00:00', strtotime($endDate));
-    }
-
-    if ($user->level == 'manager' || $user->level == 'staff') {
-      $gaji = DB::table('gaji')
-        ->select('gaji.id', 'gaji.id_transaksi', 'gaji.gaji_pokok', 'gaji.lembur', 'gaji.bonus', 'gaji.tunjangan', 'gaji.tanggal', 'gaji.pph', 'gaji.total', 'gaji.status', 'users.id as user_id', 'users.full_name as full_name', 'users.nik as nik', 'users.norek as norek', 'users.bank as bank')
-        ->leftJoin('users', 'gaji.user_id', '=', 'users.id')
-        ->where('users.company', $user->company)
-        ->whereBetween('gaji.tanggal', [$currentMonth, $nextMonth])
-        ->orderBy('gaji.created_at', 'DESC')
-        ->get();
-    } else if ($user->level == 'karyawan' || $user->level == 'trainer') {
-      $gaji = DB::table('gaji')
-        ->select('gaji.id', 'gaji.id_transaksi', 'gaji.gaji_pokok', 'gaji.lembur', 'gaji.bonus', 'gaji.tunjangan', 'gaji.tanggal', 'gaji.pph', 'gaji.total', 'gaji.status', 'users.id as user_id', 'users.full_name as full_name', 'users.nik as nik', 'users.norek as norek', 'users.bank as bank')
-        ->leftJoin('users', 'gaji.user_id', '=', 'users.id')
-        ->where('gaji.user_id', $user->id)
-        ->whereBetween('gaji.tanggal', [$currentMonth, $nextMonth])
-        ->orderBy('gaji.created_at', 'DESC')
-        ->get();
-    } else {
-      $gaji = Gaji::select('gaji.*', 'users.name as full_name')
-        ->join('users', 'gaji.user_id', '=', 'users.id')
-        ->where('gaji.user_id', $user->id)
-        ->whereBetween('gaji.tanggal', [$currentMonth, $nextMonth])
-        ->orderBy('gaji.created_at', 'DESC')
-        ->get();
-    }
-
-    // Calculate total gaji
-    $totalGaji = $gaji->sum('total');
-    $terbilang = Terbilang::make($totalGaji, ' rupiah');
-    $users = User::all(); // Get all users
-
-    // total gaji yang status terbayar
-    $gajiTerbayar = $gaji->where('status', 'terbayar');
-    $totalGajiTerbayar = $gajiTerbayar->sum('total');
-    $terbilangterbayar = Terbilang::make($totalGajiTerbayar, ' rupiah');
+    // ... (remaining code)
 
     // Get the HTML content of the view
     $html = view('account.gaji.pdf', compact('gaji', 'totalGaji', 'user', 'terbilang', 'startDate', 'endDate', 'totalGajiTerbayar', 'terbilangterbayar'))->render();
@@ -880,11 +910,14 @@ class GajiController extends Controller
     // Render the PDF
     $dompdf->render();
 
-    // Set the PDF filename
-    $fileName = 'List-Gaji-Karyawan_' . date('d-m-Y') . '.pdf';
+    // Set the response headers
+    $headers = [
+      'Content-Type' => 'application/pdf',
+      'Content-Disposition' => 'inline; filename="List-Gaji-Karyawan_' . date('d-m-Y') . '.pdf"',
+    ];
 
     // Output the generated PDF to the browser
-    return $dompdf->stream($fileName);
+    return Response::make($dompdf->output(), 200, $headers);
   }
 
   // public function SlipGaji($id)
