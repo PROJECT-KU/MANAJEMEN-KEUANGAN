cb4bf459de06f6abe69d10c7fd195d6833973f0c
berto juni <bertojunikrisnanto@gmail.com>
2024-01-03 18:33:05 +0700

    fix || fix filter uang masuk & keluar update 1 || 1.15.7

--- resources/views/account/debit/index.blade.php
+++ resources/views/account/debit/index.blade.php
@@ -31,7 +31,7 @@ List Uang Masuk | MANAGEMENT
                 <!--================== FILTER ==================-->
                 <div class="card">
                     <div class="card-header">
-                        <h4><i class="fas fa-list"></i> LIST UANG MASUK</h4>
+                        <h4><i class="fas fa-filter"></i> FILTER UANG MASUK</h4>
                     </div>
 
                     <div class="card-body">
