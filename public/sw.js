d06c24780295353137779f1645e472ce379d478a
berto juni <bertojunikrisnanto@gmail.com>
2024-04-21 18:45:28 +0700

    feat || testing pertama PWA update 4|| 1.24.0

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,11 @@
+const filesToCache = [
+    '/',
+    '/offline.html'
+];
+
 const preLoad = function () {
     return caches.open("offline").then(function (cache) {
-        // caching index and important routes
+        // Menyimpan file-file yang perlu di-cache
         return cache.addAll(filesToCache);
     });
 };
@@ -9,34 +14,34 @@ self.addEventListener("install", function (event) {
     event.waitUntil(preLoad());
 });
 
-const filesToCache = [
-    '/',
-    '/offline.html'
-];
-
 const checkResponse = function (request) {
     return new Promise(function (fulfill, reject) {
+        // Memeriksa respons dari server
         fetch(request).then(function (response) {
             if (response.status !== 404) {
                 fulfill(response);
             } else {
                 reject();
             }
-        }, reject);
+        }).catch(reject);
     });
 };
 
 const addToCache = function (request) {
-    return caches.open("offline").then(function (cache) {
-        return fetch(request).then(function (response) {
-            return cache.put(request, response);
+    // Menambahkan permintaan ke dalam cache
+    if (request.url.startsWith('http')) {
+        return caches.open("offline").then(function (cache) {
+            return fetch(request).then(function (response) {
+                return cache.put(request, response.clone());
+            });
         });
-    });
+    }
 };
 
 const returnFromCache = function (request) {
     return caches.open("offline").then(function (cache) {
         return cache.match(request).then(function (matching) {
+            // Mengembalikan dari cache jika ada, jika tidak ada, kembalikan halaman offline
             if (!matching || matching.status === 404) {
                 return cache.match("offline.html");
             } else {
@@ -47,10 +52,15 @@ const returnFromCache = function (request) {
 };
 
 self.addEventListener("fetch", function (event) {
-    event.respondWith(checkResponse(event.request).catch(function () {
-        return returnFromCache(event.request);
-    }));
-    if(!event.request.url.startsWith('http')){
+    event.respondWith(
+        // Memeriksa respons dari server, jika gagal, kembalikan dari cache
+        checkResponse(event.request).catch(function () {
+            return returnFromCache(event.request);
+        })
+    );
+
+    // Menambahkan permintaan ke dalam cache
+    if (!event.request.url.startsWith('http')) {
         event.waitUntil(addToCache(event.request));
     }
 });
