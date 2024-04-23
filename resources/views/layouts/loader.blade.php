d60be8d57c10b97ac468ef6ac296a9794aa2086e
berto juni <bertojunikrisnanto@gmail.com>
2024-04-23 10:53:53 +0700

    update || update refrest pwa di hp di semua menu update 4|| 1.24.1

--- resources/views/layouts/loader.blade.php
+++ resources/views/layouts/loader.blade.php
@@ -24,3 +24,60 @@
     }
 </style>
 <!-- END -->
+
+<!--================== MEREFRESH PWA DI HP ==================-->
+<script>
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/sw.js').then(registration => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+
+    let isRefreshing = false;
+
+    // Fungsi untuk menampilkan loader
+    function showLoader() {
+        // Tambahkan elemen loader ke dalam body
+        var loader = document.createElement('div');
+        loader.className = 'loader';
+        document.body.appendChild(loader);
+    }
+
+    // Fungsi untuk menyembunyikan loader
+    function hideLoader() {
+        // Hapus elemen loader dari body jika ada
+        var loader = document.querySelector('.loader');
+        if (loader) {
+            loader.parentNode.removeChild(loader);
+        }
+    }
+
+    // Fungsi untuk menangani refresh saat menggeser ke bawah
+    function handlePullToRefresh() {
+        // Cek apakah scroll berada di paling atas dan tidak sedang dalam proses refresh
+        if (window.scrollY === 0 && !isRefreshing) {
+            isRefreshing = true;
+            // Tampilkan loader
+            showLoader();
+
+            // Lakukan refresh halaman setelah beberapa saat
+            setTimeout(() => {
+                location.reload();
+                // Setelah proses refresh selesai, sembunyikan loader
+                hideLoader();
+                // Set isRefreshing ke false untuk memungkinkan refresh kembali
+                isRefreshing = false;
+            }, 1000); // Mengatur delay refresh selama 1 detik (1000 milidetik)
+        }
+    }
+
+    // Tambahkan event listener untuk mendeteksi gerakan menggeser ke bawah
+    window.addEventListener('scroll', handlePullToRefresh, {
+        passive: true
+    });
+</script>
+<!--================== END ==================-->
