ae6a9fb014f0b906589d00a5fbd27533efcd55e3
berto juni <bertojunikrisnanto@gmail.com>
2024-07-10 12:19:04 +0700

    fix || new version update 5|| 2.0.0

--- resources/views/layouts/loader.blade.php
+++ resources/views/layouts/loader.blade.php
@@ -95,12 +95,5 @@
     <!--================== END ==================-->
 
 </body>
-<?php
-passthru('php artisan cache:clear');
-passthru('php artisan config:clear');
-passthru('php artisan route:clear');
-passthru('php artisan view:clear');
-echo 'Cache cleared';
-?>
 
 </html>
