31566f10859dff72b949fb176d43da35b284729a
bertojuni <49329872+bertojuni@users.noreply.github.com>
2025-08-13 13:13:03 +0700

    fix || error 500 in server | 5.1.2

--- resources/views/layouts/version.blade.php
+++ resources/views/layouts/version.blade.php
@@ -170,7 +170,7 @@ $agent = new Agent();
   }
 </style>
 
-<?php $version = "5.1.0"; ?>
+<?php $version = "5.1.2"; ?>
 <footer class="main-footer" id="PwaFooter">
   <div class="footer-left">
     © <strong>Rumah Scopus Foundation</strong> {{ date("Y") }}
