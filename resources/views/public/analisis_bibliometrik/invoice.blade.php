042af21c7e066b14ffdc08154dd06cba777d9a39
bertojuni <49329872+bertojuni@users.noreply.github.com>
2025-09-04 21:17:17 +0700

    fix || error generate pdf analisis bibliometrik || 5.1.13

--- resources/views/public/analisis_bibliometrik/invoice.blade.php
+++ resources/views/public/analisis_bibliometrik/invoice.blade.php
@@ -5,8 +5,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 </head>
 
 <body>
